85c0937df72adb641aed3708b901270710cd0a88
moatazbenmansour <moatazbanmansour@gmail.com>
2026-06-28 09:54:35 +0100

    add cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
 
 
 CORS_ALLOWED_ORIGINS = [
-    "https://chatbot-project-m2036opgh-moatazbenmas-projects.vercel.app/",
+    "https://chatbot-project-m2036opgh-moatazbenmas-projects.vercel.app",
 ]
 
 
